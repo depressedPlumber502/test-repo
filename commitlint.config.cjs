21c996d0cad83a20a8d784f62d47dbaba5e7dbbc
depressedPlumber502 <depressed.plumber.502@defuse.org>
2025-01-21 22:29:41 +0800

    fix: commitlint config

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,10 +1,6 @@
 /** @type {import('@commitlint/types').UserConfig} */
 const config = {
   extends: ["@commitlint/config-conventional"],
-  rules: {
-    "body-max-line-length": [0],
-    "footer-max-line-length": [0],
-  },
 }
 
 module.exports = config
