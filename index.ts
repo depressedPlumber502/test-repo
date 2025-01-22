aa1fff6ef0b4d7b1e0ed528977e822356e8696ba
depressedPlumber502 <depressed.plumber.502@defuse.org>
2025-01-22 22:39:31 +0800

    feat!: try trig

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 function main() {
-  return "hello 1 2 feat br new lint br1 br2 abobas mabobas kekus br 123 br 1 cr no trig br br";
+  return "hello 1 2 feat br new lint br1 br2 abobas mabobas kekus br 123 br 1 cr no trig br br try trig";
 }
 
 console.log(main());
