9209a585a9206e7a87950ad5d026887dea4a85ec
depressedPlumber502 <depressed.plumber.502@defuse.org>
2025-01-22 22:58:06 +0800

    chore!: trying trig BREAKING CHANGE: tring trig.

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 function main() {
-  return "hello 1 2 feat br new lint br1 br2 abobas mabobas kekus br 123 br 1 cr no trig br br try trig 2";
+  return "hello 1 2 feat br new lint br1 br2 abobas mabobas kekus br 123 br 1 cr no trig br br try trig 2 1";
 }
 
 console.log(main());
